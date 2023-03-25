dbd31494c0c931837b45259431765c3683157c3d
Jay <jaygiri.gosai@yahoo.com>
2023-03-25 14:57:30 +0530

    React accordian

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # react-accordian
+
 Creating an accordion that toggles text content on click of the accordion header using React State and conditional rendering.
+
+The following are the steps to create an accordion in React JS:
+
+1. Display every title of accordion with body.
+2. Hide every accordion body using React State;
+3. Toggle visibility of accordion body on click of title.
+
+## DEMO
+
+![Screenshot of a demo](/src/assets/images/react-accordian.gif)
---
+++ src/utils/constants.js
@@ -0,0 +1,17 @@
+export const BLOGS = [
+  {
+    id: 1,
+    title: "Why Glass?",
+    body: "Everything tastes better in glass! And besides, glass won't leach any chemicals.",
+  },
+  {
+    id: 2,
+    title: "Who are we?",
+    body: "We are individuals within a group, within a larger group, within a still larger group, and yet we always think of ourselves as individuals. We are the collective expression of our wants, needs, fears, and desires, and everything we do serves to represent those underlying qualities.",
+  },
+  {
+    id: 3,
+    title: "Why are we here?",
+    body: "Your own purpose of existence is your life goals. These can be material or emotional goals. The purpose is based on individual values, evaluations and the experiences you had in your childhood. Throughout life, ideas about the purpose of existence can change.",
+  },
+];
